b8589fb6db3d822d32d03257fee094cc4fc8b988
Jonathan Fuerst <jonf@itu.dk>
2016-04-25 21:42:18 +0200

    try to reproduce giles2 bug

--- python/smap/drivers/example.py
+++ python/smap/drivers/example.py
@@ -3,7 +3,7 @@ Copyright (c) 2011, 2012, Regents of the University of California
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions 
+modification, are permitted provided that the following conditions
 are met:
 
  - Redistributions of source code must retain the above copyright
@@ -15,15 +15,15 @@ are met:
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
-THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 """
@@ -39,8 +39,25 @@ import logging
 
 class Driver(SmapDriver):
     def setup(self, opts):
-        self.add_timeseries('/sensor0', 'V', timeunit="s")
-        self.set_metadata('/sensor0', {
+        if self.get_collection("/4") is None:
+            self.add_collection("/4")
+        if self.get_collection("/4/4d21") is None:
+            self.add_collection("/4/4d21")
+        cl2 = self.get_collection("/4/4d21")
+        cl2['Metadata'] = {
+            'Location' : {
+                'Building': 'IT University of Copenhagen',
+                'Street': 'Rued Langgaards Vej 7',
+                'City': 'Copenhagen',
+                'Floor': "4",
+                'Room' : "4d21"
+            },
+            'Instrument': {
+                "Model": "Access Point"
+            }
+        }
+        self.add_timeseries('/4/4d21/sensor0', 'V', timeunit="s")
+        self.set_metadata('/4/4d21/sensor0', {
             'Instrument/ModelName' : 'ExampleInstrument'
             })
         self.counter = int(opts.get('StartVal', 0))
@@ -51,19 +68,19 @@ class Driver(SmapDriver):
         periodicSequentialCall(self.read).start(self.rate)
 
     def read(self):
-        self.add('/sensor0', self.counter)
+        self.add('/4/4d21/sensor0', self.counter)
         self.counter += 1
 
     def load(self, st, et, cache=None):
         d = threads.deferToThread(self.load_data, st, et)
         return d
- 
+
     def load_data(self, st, et):
         st_utc = dtutil.dt2ts(st)
         et_utc = dtutil.dt2ts(et)
         ts = int(st_utc / 120) * 120 # round down to nearest 2-min increment
         while ts <= et_utc:
-            self.add('/sensor0', ts, self.counter)
+            self.add('/4/4d21/sensor0', ts, self.counter)
             self.counter += 1
             ts += 120 # 2-min increments
-            
+
